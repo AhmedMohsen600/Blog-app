4cdfd62555c101d7486cf007e52dfb819032eb65
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-03-04 20:46:19 +0200

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_many :comments
-  has_many :likes
-  has_many :posts, foreign_key: 'author_id'
-
-  def most_recent_posts
-    posts.last(3)
+    has_many :comments, foreign_key: 'author_id', dependent: :destroy
+    has_many :likes, foreign_key: 'author_id', dependent: :destroy
+    has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  
+    validates :name, presence: true
+    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    
+    def most_recent_posts
+      posts.last(3)
+    end
   end
-end
